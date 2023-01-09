e8247d6268bc00f4ab3332b9dedf4f3c8e9c8670
Sneha <kothari.sneha05@gmail.com>
2023-01-09 11:49:44 +0800

    Extension Method

---
+++ ExtensionFunction/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ExtensionM;
+
+namespace ExtensionFunction
+{
+    public static class ExtensionFunc
+    {
+        public static string ToCurrency(this Extension str,string s)
+        {
+            string st = str.Extensions(s);
+            if (Regex.IsMatch(st,@"^\d+$"))
+            {
+                string newstr = "$" + st;
+                return newstr;
+            }
+            else
+            {
+                throw new ArgumentException("String is  not a number");
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string s = Console.ReadLine();
+            Extension ext = new Extension();
+            try
+            {
+                Console.WriteLine(ext.ToCurrency(s));
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ ExtensionM/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionM
+{
+    public class Extension
+    {
+        public string Extensions(string s)
+        {
+            return s;
+        }
+    }
+}
